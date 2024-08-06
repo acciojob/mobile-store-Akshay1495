2353ac2fa98700d855a4c8e14bdda490a00d415d
Akshay <8009akshy@gmail.com>
2024-08-06 15:18:21 +0530

    test5

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router as Router, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
 import ProductDetail from './ProductDetail';
 import Admin from './Admin';
